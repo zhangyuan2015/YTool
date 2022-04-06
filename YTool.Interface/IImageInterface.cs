5067d7b5e21e54b052db74e6b0ea0ccbe99d9265
yvan.zhang <yvan.zhang@dji.com>
2022-04-06 19:14:47 +0800

    样式，ImageTool

---
+++ YTool.Interface/IImageInterface.cs
@@ -0,0 +1,19 @@
+﻿namespace YTool.Interface
+{
+    public interface IImageInterface
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string ImageToBase64(string text);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Base64ToImage(string text);
+    }
+}
---
+++ YTool.Service/ImageService.cs
@@ -0,0 +1,17 @@
+﻿using YTool.Interface;
+
+namespace YTool.Service
+{
+    public class ImageService : IImageInterface
+    {
+        public string Base64ToImage(string text)
+        {
+            return "";
+        }
+
+        public string ImageToBase64(string text)
+        {
+            return "";
+        }
+    }
+}
