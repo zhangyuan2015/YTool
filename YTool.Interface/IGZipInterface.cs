d7ca644887fd56568c5ed9c0e3459f1b30e0ccd2
cool.zhang <cool.zhangyy@gmail.com>
2022-07-13 18:46:59 +0800

    GZip

---
+++ YTool.Interface/IGZipInterface.cs
@@ -0,0 +1,19 @@
+﻿namespace YTool.Interface
+{
+    public interface IGZipInterface
+    {
+        /// <summary>
+        /// 压缩
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Compress(string text);
+
+        /// <summary>
+        /// 解压缩
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Decompress(string text);
+    }
+}
